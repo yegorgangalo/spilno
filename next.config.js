6ef01439e4f64e72734767a745c6bde568df6c92
yegorgangalo <gangaloyegor@gmail.com>
2023-07-12 01:00:49 +0300

    add aws ses

--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
+      BASE_URL: process.env.BASE_URL,
       DATABASE_URL: process.env.DATABASE_URL,
       NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
       NEXTAUTH_URL: process.env.NEXTAUTH_URL,
       JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-      NODEMAILER_HOST: process.env.NODEMAILER_HOST,
-      NODEMAILER_PORT: process.env.NODEMAILER_PORT,
-      NODEMAILER_USER: process.env.NODEMAILER_USER,
-      NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
       SEED_ADMIN_LOGIN: process.env.SEED_ADMIN_LOGIN,
       SEED_ADMIN_PASSWORD: process.env.SEED_ADMIN_PASSWORD,
+      ACCESS_KEY_AWS: process.env.ACCESS_KEY_AWS,
+      SECRET_KEY_AWS: process.env.SECRET_KEY_AWS,
     },
 }
 
